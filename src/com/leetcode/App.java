e7ec183ac601b281405009edb97334af1a4108c4
SumSmile <juna3066@gmail.com>
2022-03-11 17:09:22 +0800

    基础功能添加

---
+++ src/com/leetcode/App.java
@@ -0,0 +1,57 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class App {
+    private ArrayList<String> classNames = new ArrayList<>();
+
+    private static final String SCAN_PACKAGE = "com.leetcode";
+
+    /**
+     * 递归扫描所有class文件
+     * @param packageName
+     */
+    private void doScanner(String packageName) {
+        //将所有的包路径转换为文件路径
+        URL url = this.getClass().getClassLoader().getResource(packageName.replaceAll("\\.", "/"));
+        File dir = new File(url.getFile());
+        for (File file : dir.listFiles()) {
+            //如果是文件夹，继续递归
+            if(file.isDirectory()){
+                doScanner(packageName + "." + file.getName());
+            }else{
+                if (file.getName().endsWith(".class")){
+                    classNames.add(packageName + "." + file.getName().replace(".class", "").trim());
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test() throws ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        String problem = "53";
+        String cName = "";
+        doScanner(SCAN_PACKAGE);
+        //classNames.forEach(System.out::println);
+        for (String className : classNames) {
+            if (className.contains(problem)){
+                cName = className;
+                break;
+            }
+        }
+        Class<?> clazz = Class.forName(cName);
+        Object o = clazz.newInstance();
+
+        Method solute = clazz.getMethod("solute", null);
+        if (solute!=null){
+            solute.invoke(o);
+        }
+    }
+
+}
---
+++ src/com/leetcode/studyplan/datastructures/c01arr/p053/Solution.java
@@ -0,0 +1,7 @@
+package com.leetcode.studyplan.datastructures.c01arr.p053;
+
+public class Solution {
+    public void solute() {
+        System.out.println("com.leetcode.studyplan.datastructures.c01arr.p053 Solution.solute");
+    }
+}
