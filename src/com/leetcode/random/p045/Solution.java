dea7491c172f78c5a2311b59d87a8532a64755d3
SumSmile <juna3066@gmail.com>
2022-04-13 09:37:04 +0800

    704 有序数组，二分查找

--- src/com/leetcode/random/p045/Solution.java
+++ src/com/leetcode/random/p045/Solution.java
@@ -36,9 +36,10 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-//        int[] arr = new  int[]{100,3,0,0,3,5,2,9,1,4};
-        int[] arr = new  int[]{4,1,1,3,1,1,1};
-        //timeout
+        int[] arr = new  int[]{5,9,3,2,1,0,2,3,3,1,0,0};
+//        int[] arr = new  int[]{2,3,0,0,3,5,2,9,1,4};
+//        int[] arr = new  int[]{4,1,1,3,1,1,1};
+
 
 
         int jump = new Solution().jump(arr);
