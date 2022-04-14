70916328c5628d5ddd71fb0aeaee2f8c94f0fd72
SumSmile <juna3066@gmail.com>
2022-04-14 11:13:59 +0800

    640.求解方程  ok-待优化 结果：t8 n5

--- src/com/leetcode/random/p640/Solution.java
+++ src/com/leetcode/random/p640/Solution.java
@@ -6,43 +6,65 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
+
+
+    private void demoTest1(String left) {
+        String[] ls = left.split("-|\\+");
+        for (String l : ls) {
+            System.out.println(l);
+        }
+    }
+
+
+    public static void main(String[] args) {
+//        String str = "equation = \"100x+5-3+x=6+x+296+50x\"";
+        String str = "x+5-3+x=6+x-2";
+        String s = new Solution().solveEquation(str);
+        System.out.println(s);
+    }
+
     public String solveEquation(String equation) {
-        String s = equation.split("\"")[1];
-        String[] split = s.split("=");
+        //String s = equation.split("\"")[1];
+        String[] split = equation.split("=");
         String left = split[0];
         String right = split[1];
 
-        System.out.println("right = " + right);
-        System.out.println("left = " + left);
-
-        //demoTest2(left);
-        //demoTest1(left);
-
-
-
         ArrayList<String> leftXs = new ArrayList<>();
         ArrayList<String> rightNs = new ArrayList<>();
-        demoTest2(leftXs,rightNs,left,true);
-        demoTest2(leftXs,rightNs,right,false);
+        eqMove(leftXs,rightNs,left,true);
+        eqMove(leftXs,rightNs,right,false);
+
+        int preSNum = 0;
+        //[0-9] \d
+        String pattern = "[0-9]";
+        Pattern c = Pattern.compile(pattern);
 
         for (String str : leftXs) {
-            System.out.print(str);
+            String repX = c.matcher(str).find() ? "" : "1";
+            String x = str.replace("x", repX);
+            preSNum = preSNum + Integer.valueOf(x);
         }
-        System.out.print(" = " );
-//        for (String str : rightNs) {
-//            System.out.print(str);
-//        }
 
         int sum = 0;
         for (String rightN : rightNs) {
             sum += Integer.parseInt(rightN);
         }
-        System.out.print(sum);
 
-        return null;
+        String res = "";
+        if (preSNum==0){
+            if(sum==0){
+                res="Infinite solutions";
+            }else{
+                res="No solution";
+            }
+        }else{
+            res="x="+(sum/preSNum);
+        }
+
+        return res;
     }
 
-    private void demoTest2(List<String> leftXs,List<String> rightNs, String arr,Boolean isX ) {
+    private void eqMove(List<String> leftXs,List<String> rightNs, String arr,Boolean isX ) {
         /**
          *
          * ？ 零或者1
@@ -54,7 +76,6 @@ public class Solution {
         Matcher matcher = c.matcher(arr);
         while (matcher.find()){
             String group = matcher.group();
-            System.out.println(group);
             if (isX){
                 if (group.contains("x")){
                     leftXs.add(group);
@@ -69,8 +90,6 @@ public class Solution {
                 }
             }
         }
-
-        System.out.println("");
     }
 
     public String getXFNumStr(String numStr){
@@ -83,17 +102,5 @@ public class Solution {
             res = "-" + numStr;
         }
         return res;
-    }
-
-    private void demoTest1(String left) {
-        String[] ls = left.split("-|\\+");
-        for (String l : ls) {
-            System.out.println(l);
-        }
-    }
-
-    public static void main(String[] args) {
-        String str = "equation = \"100x+5-3+x=6+x-2\"";
-        new Solution().solveEquation(str);
     }
 }
