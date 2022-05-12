79418aee9869d570eaf4bbed8071ee5e1fe1c296
jun <juna3066@qq.com>
2022-05-13 01:12:04 +0800

    206. 反转链表-官方方法1-迭代

--- src/com/leetcode/hot/n206/Solution.java
+++ src/com/leetcode/hot/n206/Solution.java
@@ -77,11 +77,35 @@ public class Solution {
         return resHead;
     }
 
+    /***
+     * @param head
+     * @return
+     */
+    public ListNode officialReverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur!=null){
+            ListNode next = cur.next;
+            //当前的下一个指向 前一个没有被结构保存的数据 下一个再迭代要用，因此提前保存
+            cur.next = pre;
+
+            //移动
+            //当前变成 前元素
+            pre = cur;
+            //当前的指向下一个
+            cur = next;
+        }
+        //便利结束 pre是最后一个 cur是空
+        return pre;
+    }
+
+
     @Test
     public void test() {
         ListNode node = new ListNode(1, new ListNode(2,new ListNode(3)));
         //printListNode(node);
-        ListNode res = reverseList2(node);
+        ListNode res = officialReverseList(node);
         printListNode(res);
     }
 
