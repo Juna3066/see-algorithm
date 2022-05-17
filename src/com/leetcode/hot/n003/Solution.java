af354ce0229765856f228b7ace6f42b5c1c6cd02
SumSmile <juna3066@gmail.com>
2022-05-17 11:58:06 +0800

    3. 无重复字符的最长子串-评论题解

--- src/com/leetcode/hot/n003/Solution.java
+++ src/com/leetcode/hot/n003/Solution.java
@@ -2,10 +2,32 @@ package com.leetcode.hot.n003;
 
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Solution {
-    public int lengthOfLongestSubstring(String s) {
+
+    /**
+     * 突然感觉做题有时候后难点是 输入yong'li
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstringC1(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0, start = 0;
+        for (int end = 0; end < s.length(); end++) {
+            char ch = s.charAt(end);
+            if (map.containsKey(ch)){
+                //"abba"
+                start = Math.max(map.get(ch)+1,start);
+            }
+            max = Math.max(max,end - start + 1);
+            map.put(ch,end);
+        }
+        return max;
+    }
+
+    public int lengthOfLongestSubstringM1(String s) {
         int start = 0, maxLen = 0;
         HashSet<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
@@ -18,13 +40,13 @@ public class Solution {
                 }
                 set.clear();
                 //这个开始的坐标移动是一个难点
-                if (c==s.charAt(i-1)){
-                    //挨着
-                    start = i;
-                }else {
-                    //重复的不挨着
+//                if (c==s.charAt(i-1)){
+//                    //挨着
+//                    start = i;
+//                }else {
+                    //重复的不挨着  下标可以这样理解 0 前面有0个元素，1前面有个一个元素
                     start = start + s.substring(start).indexOf(c) + 1;
-                }
+//                }
 
                 char[] chs = s.substring(start, i + 1).toCharArray();
                 for (char ch : chs) {
@@ -37,7 +59,7 @@ public class Solution {
 
     @Test
     public void test() {
-        int len = lengthOfLongestSubstring("obovodc");
+        int len = lengthOfLongestSubstringM1("abcood");
         System.out.println("len = " + len);
     }
 }
